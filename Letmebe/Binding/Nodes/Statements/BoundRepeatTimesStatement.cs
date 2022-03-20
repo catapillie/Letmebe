3d604c73af79316d37f8e7113cc555d675cf99b4
catapillie <espritox@gmail.com>
2022-03-20 23:23:27 +0100

    Implement lowering for repeat-times statement

--- Letmebe/Binding/Nodes/Statements/BoundRepeatTimesStatement.cs
+++ Letmebe/Binding/Nodes/Statements/BoundRepeatTimesStatement.cs
@@ -15,6 +15,50 @@ namespace Letmebe.Binding.Nodes {
             yield return Statement;
         }
 
-        // TODO: Lowering (needs assignment expression)
+        public override BoundStatement Lowered() {
+            /*
+             * ...
+             * let amount be int <value>;
+             * @repeat
+             * <statement>
+             * amount <- amount - 1;
+             * goto @repeat if amount > 0
+             * ...
+             */
+
+            var amountSymbol = new BoundSymbol(BoundPrimitiveType.IntegerPrimitive, "amount");
+
+            var amountDeclaration = new BoundVariableDefinitionStatement(amountSymbol, Amount);
+            var repeatLabel = new BoundLabelStatement("@repeat");
+
+            // (amount <- (amount - (1)))
+            var amountDecrementStatement = new BoundExpressionStatement(
+                new BoundAssignmentExpression(
+                    amountSymbol,
+                    new BoundBinaryOperation(
+                        amountSymbol,
+                        new(Lexing.TokenKind.MINUS, BoundBinaryOperator.Operation.IntegerSubtraction, BoundPrimitiveType.IntegerPrimitive),
+                        new BoundLiteral(BoundPrimitiveType.IntegerPrimitive, 1)
+                    )
+                )
+            );
+
+            // (amount > (0))
+            var amountCheckExpression = new BoundBinaryOperation(
+                amountSymbol,
+                new(Lexing.TokenKind.RIGHTCHEVRON, BoundBinaryOperator.Operation.IntegerGreaterThan, BoundPrimitiveType.IntegerPrimitive, BoundPrimitiveType.BooleanPrimitive),
+                new BoundLiteral(BoundPrimitiveType.IntegerPrimitive, 0)
+            );
+
+            var gotoRepeat = new BoundConditionalGotoStatement(repeatLabel, amountCheckExpression, negated: false);
+
+            return new BoundBlockStatement(new[] {
+                amountDeclaration,
+                repeatLabel,
+                Statement.Lowered(),
+                amountDecrementStatement,
+                gotoRepeat,
+            });
+        }
     }
 }
